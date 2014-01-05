48ab332dffed32edebc59815509b09b3bae71eda
Florent Biville <f.biville@lateral-thoughts.com>
2014-01-05 19:05:15 +0100

    Oops, copy CNAME as well!

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,8 @@ module.exports = function(grunt) {
 					{expand: true, src: ['css/**'], dest: finalSiteFolder},
 					{expand: true, src: ['font/**'], dest: finalSiteFolder},
 					{expand: true, src: ['img/**'], dest: finalSiteFolder},
-					{expand: true, src: ['js/**'], dest: finalSiteFolder}
+					{expand: true, src: ['js/**'], dest: finalSiteFolder},
+					{expand: true, src: ['CNAME'], dest: finalSiteFolder}
 				]
 			}
 		}

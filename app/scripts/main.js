8ca3d8119c7b1798cdd8a02a4e1a23eba4c197f7
Nicolas REY <nicolas.rey.3k@gmail.com>
2014-06-16 00:19:47 +0200

    Add rollover on offer, and tiny CSS improvements.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('\'Allo \'Allo!');
+
+
+// Over state of Studio, Swat, Training.
+var offerORollover = function(offerName) {
+  $(".lt-offer").hover(function() {
+    $(this).find(".lt-offer-pic").removeClass(offerName).addClass("icon-lt_offer_rollover");
+  }, function() {
+    $(this).find(".lt-offer-pic").addClass(offerName).removeClass("icon-lt_offer_rollover");
+  });
+};
+
+offerORollover("icon-lt_studio");
+offerORollover("icon-lt_training");
+offerORollover("icon-lt_swat");

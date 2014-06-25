4fd8ff937b537c7544f429b7042a69ae10078dce
Jonathan Dray <jdray@gsexterne.com>
2014-06-25 13:04:07 +0200

    style(js validation): fix jshint on main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,17 @@
+'use strict';
+
 console.log('\'Allo \'Allo!');
 
 
 // Over state of Studio, Swat, Training.
 var offerRollover = function(offerName) {
-  $(".lt-" + offerName).hover(function() {
-    $(this).find(".lt-offer-pic").removeClass("icon-lt_" + offerName).addClass("icon-lt_offer_rollover");
+  $('.lt-' + offerName).hover(function() {
+    $(this).find('.lt-offer-pic').removeClass('icon-lt_' + offerName).addClass('icon-lt_offer_rollover');
   }, function() {
-    $(this).find(".lt-offer-pic").addClass("icon-lt_" + offerName).removeClass("icon-lt_offer_rollover");
+    $(this).find('.lt-offer-pic').addClass('icon-lt_' + offerName).removeClass('icon-lt_offer_rollover');
   });
 };
 
-offerRollover("studio");
-offerRollover("training");
-offerRollover("swat");
+offerRollover('studio');
+offerRollover('training');
+offerRollover('swat');

d6b8ac5cc9541b50a4115c1a1aa4ce195561de57
Jonathan Dray <jonathan.dray@gmail.com>
2014-07-31 21:56:29 +0200

    refactor(wow.js): wow js lib with bower

--- app/js/main.js
+++ app/js/main.js
@@ -17,7 +17,7 @@ require.config({
 
 require(
 	[
-		'jquery', 'moment', 'rssAggregator', 'underscore', 'localLinkFixer', 'wow'
+		'jquery', 'moment', 'rssAggregator', 'underscore', 'localLinkFixer',
 		'text!tpl/expanded_blog_post.tpl', 'text!tpl/footer_blog_post.tpl'
 	], 
 	function($, moment, rss, _, localLinkFixer, expandedPostTemplate, footerPostTemplate) {

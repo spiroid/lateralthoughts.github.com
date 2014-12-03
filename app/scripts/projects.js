c7e9a92ed027a50af781f11638f56201b518606b
Jonathan Dray <jonathan.dray@gmail.com>
2014-12-03 21:48:21 +0100

    New deploy!

--- app/scripts/projects.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-$('.project .description').readmore({
-    moreLink: '<a href="#" class="see-more text-center"><i class="fa fa-plus-circle fa-lg"></i> Voir plus</a>',
-    lessLink: '<a href="#" class="see-less text-center"><i class="fa fa-minus-circle fa-lg"></i> Fermer</a>',
-    speed: 200,
-    maxHeight: 100
-});

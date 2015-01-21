e40a1cb77a3eba6d4822227129a62eded2bb517d
Jonathan Dray <jonathan.dray@gmail.com>
2015-01-21 01:19:44 +0100

    chore(deploy): new script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# Get current branch
+BRANCH=`git symbolic-ref --short HEAD`
+
 # Remove old generated files if they exist
 rm -rf generated
+
+# Save uncommited changes
+git stash save -u 'Before deployement'
 
 # Launch build command
 gulp
@@ -12,14 +19,23 @@ cp -a dist generated
 git add -A generated
 git commit -m "New deploy!"
 
-# Remove deploy branch
+# Remove old deploy branch
 git branch -D deploy
 
-# Subtree
+# Subtree - create a temporary branch deploy from the content of the generated directory
 git subtree split --prefix=generated --branch deploy
 
-# Propagate modification to the remote repository
-git push -f origin deploy:master
+# Sync master locally
+git checkout master && git pull origin master
 
-# Reset
-git reset --hard origin/dev
+# Merge all modifications in master branch
+git checkout deploy && git merge -s ours master
+git checkout master && git merge deploy
+
+# Push modifications to the remote repository, on master branch
+git push origin master
+
+# restore old context
+git checkout ${BRANCH}
+git reset --hard HEAD^
+git stash pop
